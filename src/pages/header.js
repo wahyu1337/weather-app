980152a010b807d47bba49bc7d693385a58aa076
wahyu1337 <wahyuisnaeni2002@gmail.com>
2026-05-05 10:18:36 +0800

    update & adjust dark mode
    update content layour and adjust into dark mode background

--- src/pages/header.js
+++ src/pages/header.js
@@ -2,7 +2,7 @@ export default function Header() {
     // header parent
     const header = document.querySelector("#header");
     // header child
-    const titleHeader = document.createElement("h1");
+    const titleHeader = document.createElement("p");
     titleHeader.textContent = "Ways - Weather App";
     titleHeader.classList.add("header-title");
 
