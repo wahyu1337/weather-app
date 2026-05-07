7c9a8f8f17fc7a987b1a36c03ed2ae10bb7ef386
wahyu1337 <wahyuisnaeni2002@gmail.com>
2026-05-07 23:41:01 +0800

    Update theme mode function and some hover styling

--- src/components/event-handler.js
+++ src/components/event-handler.js
@@ -4,24 +4,31 @@ import logs from "./logs.js";
 let currentDarkMode = true;
 
 // event listener
-// darkmode
+// darkmode button
 function darkMode() {
     const darkmode = document.querySelector("#darkmode");
+    const themeMode = document.getElementById("theme");
     // event listener
     darkmode.addEventListener("click", () => {
     const container = document.getElementById("container");
+    const inputBox = document.getElementById("inputContent");
         if (currentDarkMode) {
             container.style.backgroundColor = "white";
-            container.style.color = "black"
+            container.style.color = "black";
+            themeMode.textContent = "light"
+            inputBox.classList.add("light");
             return currentDarkMode = false;
         } else {
             container.style.backgroundColor = "black";
             container.style.color = "white";
+            themeMode.textContent = "dark_mode";
+            inputBox.classList.remove("light");
             return currentDarkMode = true;
         }
     }) 
 }
 
+// search button
 function searchBtn() {
     const btnSearch = document.getElementById("btnSearch");
     btnSearch.addEventListener("click", () => {
